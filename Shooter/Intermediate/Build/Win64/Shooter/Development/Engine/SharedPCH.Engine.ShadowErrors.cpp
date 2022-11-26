586d62a22aac8648d925e5584ff73b5b6d9a45ae
Ivan Tsoy <98018454+DesPises@users.noreply.github.com>
2022-11-26 15:05:39 +0300

    Bug fix
    - Changed algorithm of level switching
    - Project settings adjusted

---
+++ Shooter/Intermediate/Build/Win64/Shooter/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30147
+#include "C:/Users/Professional/Desktop/Projects/2 - Shooter/ShooterProject/Shooter/Intermediate/Build/Win64/Shooter/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
