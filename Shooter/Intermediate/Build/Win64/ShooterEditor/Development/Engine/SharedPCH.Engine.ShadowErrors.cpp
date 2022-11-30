ec0f4db0b6bff83ef6c46f520020a3dbff55cb03
Ivan Tsoy <98018454+DesPises@users.noreply.github.com>
2022-11-30 21:04:35 +0300

    Screenshots added

--- Shooter/Intermediate/Build/Win64/ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Shooter/Intermediate/Build/Win64/ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30146
-#include "C:/Unreal Engine/Projects/2 - Shooter/ShooterProject/Shooter/Intermediate/Build/Win64/ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.29.30147
+#include "C:/Users/Professional/Desktop/Projects/2 - Shooter/ShooterProject/Shooter/Intermediate/Build/Win64/ShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
